d2a47a54a7ea886770e0949e2df966a0b15bd344
Jirka B <j.borovec+github@gmail.com>
2024-11-18 13:25:28 +0100

    linter

--- src/litmodels/__init__.py
+++ src/litmodels/__init__.py
@@ -7,8 +7,6 @@ from litmodels.__about__ import *  # noqa: F401, F403
 _PACKAGE_ROOT = os.path.dirname(__file__)
 _PROJECT_ROOT = os.path.dirname(_PACKAGE_ROOT)
 
-from litmodels.cloud_io import upload_model, download_model
+from litmodels.cloud_io import download_model, upload_model
 
-__all__ = [
-    "download_model", "upload_model"
-]
+__all__ = ["download_model", "upload_model"]
