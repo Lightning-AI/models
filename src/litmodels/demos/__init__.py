88e762fa120f7e4841d5d79a862f2daf3347c421
Jirka B <j.borovec+github@gmail.com>
2024-11-25 15:10:56 +0100

    DemoModel

--- src/litmodels/demos/__init__.py
+++ src/litmodels/demos/__init__.py
@@ -5,10 +5,10 @@ from lightning_utilities import module_available
 __all__ = []
 
 if module_available("lightning"):
-    from lightning.pytorch.demos.boring_classes import BoringModel
+    from lightning.pytorch.demos.boring_classes import BoringModel, DemoModel
 
-    __all__ += ["BoringModel"]
+    __all__ += ["BoringModel", "DemoModel"]
 elif module_available("pytorch_lightning"):
-    from pytorch_lightning.demos.boring_classes import BoringModel
+    from pytorch_lightning.demos.boring_classes import BoringModel, DemoModel
 
-    __all__ += ["BoringModel"]
+    __all__ += ["BoringModel", "DemoModel"]
