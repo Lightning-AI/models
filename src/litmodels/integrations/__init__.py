44c9759d286755436b615103deb99e1b28fea0d0
Jirka B <j.borovec+github@gmail.com>
2025-03-10 12:00:03 +0100

    releasing `0.1.1`

--- src/litmodels/integrations/__init__.py
+++ src/litmodels/integrations/__init__.py
@@ -1,1 +1,4 @@
 """Integrations with training frameworks like PyTorch Lightning, TensorFlow, and others."""
+
+from litmodels.integrations.checkpoints import LitModelCheckpoint
+from litmodels.integrations.duplicate import duplicate_hf_model
