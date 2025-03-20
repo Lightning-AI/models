54d7359464392b53c225b87a139f865260c5d129
Jirka B <j.borovec+github@gmail.com>
2025-03-20 16:54:40 +0100

    note

--- tests/integrations/__init__.py
+++ tests/integrations/__init__.py
@@ -8,11 +8,11 @@ from litmodels.integrations.imports import (
 
 _SKIP_IF_LIGHTNING_MISSING = pytest.mark.skipif(not _LIGHTNING_AVAILABLE, reason="Lightning not available")
 _SKIP_IF_LIGHTNING_BELLOW_2_5_1 = pytest.mark.skipif(
-    not _LIGHTNING_GREATER_EQUAL_2_5_1, reason="Lightning not available"
+    not _LIGHTNING_GREATER_EQUAL_2_5_1, reason="Lightning without integration introduced in 2.5.1"
 )
 _SKIP_IF_PYTORCHLIGHTNING_MISSING = pytest.mark.skipif(
     not _PYTORCHLIGHTNING_AVAILABLE, reason="PyTorch Lightning not available"
 )
 _SKIP_IF_PYTORCHLIGHTNING_BELLOW_2_5_1 = pytest.mark.skipif(
-    not _PYTORCHLIGHTNING_GREATER_EQUAL_2_5_1, reason="PyTorch Lightning not available"
+    not _PYTORCHLIGHTNING_GREATER_EQUAL_2_5_1, reason="PyTorch Lightning without integration introduced in 2.5.1"
 )
