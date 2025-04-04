b4dec259c85a24c8b22c0cfd3763ddd425de235a
Jirka B <j.borovec+github@gmail.com>
2025-04-04 11:28:35 -0400

    mock

--- tests/integrations/test_checkpoints.py
+++ tests/integrations/test_checkpoints.py
@@ -3,16 +3,8 @@ import re
 from unittest import mock
 
 import pytest
-from litmodels.integrations.checkpoints import get_model_manager
 
 from tests.integrations import _SKIP_IF_LIGHTNING_MISSING, _SKIP_IF_PYTORCHLIGHTNING_MISSING
-
-
-@pytest.fixture(autouse=True)
-def reset_model_manager():
-    get_model_manager.cache_clear()
-    # Optionally, call it once to initialize immediately
-    return get_model_manager()
 
 
 @pytest.mark.parametrize(
