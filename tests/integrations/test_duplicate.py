4a283f7a519cbe9be9a4419c1130d6ed6e9196d4
Jirka B <j.borovec+github@gmail.com>
2025-04-07 20:56:29 +0200

    mocks

--- tests/integrations/test_duplicate.py
+++ tests/integrations/test_duplicate.py
@@ -1,10 +1,7 @@
 import os
 from unittest import mock
 
-from lightning_sdk.utils.resolve import _resolve_teamspace
 from litmodels.integrations.duplicate import duplicate_hf_model
-
-from tests.integrations import LIT_ORG, LIT_TEAMSPACE
 
 
 @mock.patch("litmodels.integrations.duplicate.snapshot_download")
@@ -12,13 +9,10 @@ from tests.integrations import LIT_ORG, LIT_TEAMSPACE
 def test_duplicate_hf_model(mock_upload_model, mock_snapshot_download, tmp_path):
     """Verify that the HF model can be duplicated to the teamspace"""
 
+    hf_model = "google/t5-efficient-tiny"
     # model name with random hash
     model_name = f"litmodels_hf_model+{os.urandom(8).hex()}"
-    teamspace = _resolve_teamspace(org=LIT_ORG, teamspace=LIT_TEAMSPACE, user=None)
-    org_team = f"{teamspace.owner.name}/{teamspace.name}"
-
-    hf_model = "google/t5-efficient-tiny"
-    duplicate_hf_model(hf_model=hf_model, lit_model=f"{org_team}/{model_name}", local_workdir=str(tmp_path))
+    duplicate_hf_model(hf_model=hf_model, lit_model=model_name, local_workdir=str(tmp_path))
 
     mock_snapshot_download.assert_called_with(
         repo_id=hf_model,
@@ -30,7 +24,7 @@ def test_duplicate_hf_model(mock_upload_model, mock_snapshot_download, tmp_path)
         max_workers=os.cpu_count(),
     )
     mock_upload_model.assert_called_with(
-        name=f"{org_team}/{model_name}",
+        name=f"{model_name}",
         path=tmp_path / hf_model.replace("/", "_"),
         metadata={"hf_model": hf_model, "litModels_integration": "duplicate_hf_model"},
         verbose=1,
